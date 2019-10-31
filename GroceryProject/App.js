f970c77aefd46ea2a6eae8e2b35fcd30a852f22b
TrentBecker01 <trent4becker@gmail.com>
2019-10-30 19:44:53 -0500

    [Added bottom nav bar]

--- GroceryProject/App.js
+++ GroceryProject/App.js
@@ -1,13 +1,44 @@
-import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+/* -- Bottom navigation: quick navigation in bottom bar */
+import * as React from 'react';
+import { BottomNavigation, Text } from 'react-native-paper';
 
-export default class HelloWorldApp extends Component {
+const StateRoute = () => <Text>Musaaic</Text>;
+
+const ClientRoute = () => <Text>Albums</Text>;
+
+const SettingRoute = () => <Text>Recents</Text>;
+
+const LogoutRoute = () => <Text>logout</Text>
+
+export default class BottomNavigatonComponent extends React.Component {
+  state = {
+    index: 0,
+    routes: [
+      { key: 'state', title: 'Statistics', icon: 'queue-music' },
+      { key: 'client', title: 'Client', icon: 'album' },
+      { key: 'setting', title: 'Setting', icon: 'recent_actors' },
+      { key: 'logout', title: 'Logout', icon: 'recent_actors' },
+    ],
+  };
+
+  // Vi sao ham 1 tham so => khi goi khong co tham so
+  // truyen index vao setState co {} ????
+  _handleIndexChange = index => this.setState({ index });
+
+  _renderScene = BottomNavigation.SceneMap({
+    state: StateRoute,
+    client: ClientRoute,
+    setting: SettingRoute,
+    logout: LogoutRoute,
+  });
+
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Hello, world!</Text>
-	<Text>hi trent</Text>
-      </View>
+      <BottomNavigation
+        navigationState={this.state}
+        onIndexChange={this._handleIndexChange}
+        renderScene={this._renderScene}
+      />
     );
   }
 }
