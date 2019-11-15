8c20094ac0e76dd0e0dea4b7915e14aff2f3681c
TrentBecker01 <trent4becker@gmail.com>
2019-11-15 03:47:42 -0600

    [Added cards to shops/exchanges feed]

--- GroceryProject/components/feedComponents/exchanges.js
+++ GroceryProject/components/feedComponents/exchanges.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
-
+import { View, Text, StyleSheet, Image, ScrollView } from 'react-native';
+import {Card, Button, ListItem, Icon} from 'react-native-elements'
 
 export default class ExchangesComponent extends React.Component {
     render() {
-        return (<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text style={{ fontSize: 100 }}>{1}</Text>
-    </View>);
+        return (<ScrollView>
+            <Card>
+              <Text style={{fontSize: 20, fontWeight: "bold", marginBottom: 0}}>4 Bananas</Text>
+              <Text style={{marginBottom: 0}}>
+                8:30pm | David Y. | Free
+              </Text>
+              <Text style={{marginBottom: 10, color: 'gray'}}>Skyloft Lobby</Text>
+              <Button
+                icon={<Icon name='message' color='#ffffff' />}
+                buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+                title='Message' />
+            </Card>
+          </ScrollView>);
     }
 }
--- GroceryProject/components/feedComponents/shops.js
+++ GroceryProject/components/feedComponents/shops.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
-
+import { View, Text, StyleSheet, Image, ScrollView } from 'react-native';
+import {Card, Button, ListItem, Icon} from 'react-native-elements'
 
 export default class ShopsComponent extends React.Component {
     render() {
-        return (<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text style={{ fontSize: 200 }}>{25}</Text>
-    </View>);
+        return (<ScrollView>
+            <Card image={require('./../../assets/heb.png')}>
+              <Text style={{fontSize: 20, fontWeight: "bold", marginBottom: 0}}>HEB Run!</Text>
+              <Text style={{marginBottom: 0}}>
+                5:30pm | Jessica B.
+              </Text>
+              <Text style={{marginBottom: 10, color: 'gray'}}>Hancock Center</Text>
+              <Button
+                icon={<Icon name="add" color='#ffffff' />}
+                buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+                title='Join This Group'
+                />
+            </Card>
+          </ScrollView>);
     }
 }
