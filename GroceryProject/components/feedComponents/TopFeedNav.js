6dd2f938a1e5be5bc4aef4eb2e3fb5ce7aabb757
TrentBecker01 <trent4becker@gmail.com>
2019-11-16 10:26:04 -0600

    [Fixed FAB]

--- GroceryProject/components/feedComponents/TopFeedNav.js
+++ GroceryProject/components/feedComponents/TopFeedNav.js
@@ -23,7 +23,7 @@ export default class TopFeedNav extends React.Component {
     render() {
         return (
           <View style={{ flex: 1 }}>
-              <Image source={require('./../../assets/FAB.png')} style={{width: 56, height: 56, position: "absolute", bottom: 16, right: 16, zIndex: 999}}></Image>
+              <Image source={require('./../../assets/plusFAB.png')} style={{width: 56, height: 56, position: "absolute", bottom: 16, right: 16, zIndex: 999}}></Image>
                 <TopBarNav
                     // routeStack and renderScene are required props
                     routeStack={ROUTESTACK}
